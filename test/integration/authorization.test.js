9416f60e83f33f98eba33db892f02b0d92b0b1e4
lucas.klaassen@me.com <lucas.klaassen@me.com>
2018-01-11 08:16:21 -0800

    Added audience validation and specs

--- test/integration/authorization.test.js
+++ test/integration/authorization.test.js
@@ -4,34 +4,51 @@ import authorization from './../support/authorization';
 const expect = require('chai').expect;
 
 describe("Test the authorizer", function(){
-	let methodArn = 'arn:aws:execute-api:random-region:random-account-id:random-api-id/development/GET/test';
-	
-	it('should deny access when auth token is not valid', function(done){
-		let event = { 
-			type: 'TOKEN',
-			authorizationToken: 'Bearer test',
-			methodArn: methodArn
-		}
+  let methodArn = 'arn:aws:execute-api:random-region:random-account-id:random-api-id/development/GET/test';
+  
+  it('should deny access when auth token is not valid', function(done){
+    let event = { 
+      type: 'TOKEN',
+      authorizationToken: 'Bearer test',
+      methodArn: methodArn
+    }
 
-		auth0Authorizer(event).then(successMessage => {
-			console.log(successMessage);
-		}).catch(err => {
-			expect(err.message).to.equal('jwt malformed');
-		}).then(done, done);
-	})
-	it('should allow access when auth token is valid', function(done){
-		authorization.getAccessToken().then(accessTokenHeader => {
-			let event = {
-				type: 'TOKEN',
-				authorizationToken: accessTokenHeader,
-				methodArn: methodArn
-			}
+    auth0Authorizer(event).then(successMessage => {
+      throw new Error('succesful response');
+    }).catch(err => {
+      expect(err.message).to.equal('jwt malformed');
+    }).then(done, done);
+  });
+  
+  it('should deny access when audience within token is not valid', function(done){
+    authorization.getAccessToken(process.env.BAD_AUDIENCE).then(accessTokenHeader => {
+      let event = {
+        type: 'TOKEN',
+        authorizationToken: accessTokenHeader,
+        methodArn: methodArn
+      }
+  
+      auth0Authorizer(event).then(successMessage => {
+        throw new Error('succesful response');
+      }).catch(err => {
+        expect(err.message).to.equal('jwt audience invalid. expected: https://test.npm-authorizer.com');
+      }).then(done, done);
+    });
+  });
+  
+  it('should allow access when auth token is valid', function(done){
+    authorization.getAccessToken().then(accessTokenHeader => {
+      let event = {
+        type: 'TOKEN',
+        authorizationToken: accessTokenHeader,
+        methodArn: methodArn
+      }
 
-			auth0Authorizer(event).then(successMessage => {
-				expect(successMessage.policyDocument.Statement[0].Effect).to.equal('Allow');
-			}).catch(err => {
-				console.log(err);
-			}).then(done, done);
-		});
-	});
+      auth0Authorizer(event).then(successMessage => {
+        expect(successMessage.policyDocument.Statement[0].Effect).to.equal('Allow');
+      }).catch(err => {
+        throw err;
+      }).then(done, done);
+    });
+  });
 });
--- test/support/authorization.js
+++ test/support/authorization.js
@@ -3,24 +3,28 @@
 import utilities from './../../lib/utilities';
 
 var authorization = {
-	getAccessToken: function () {
-		let options = {
-			method: 'POST',
-			url: `${process.env.AUTH0_DOMAIN}oauth/token`,
-			headers: { 'content-type': 'application/json' },
-			body: `{
-				"client_id": "${process.env.AUTH0_CLIENT_ID}",
-				"client_secret": "${process.env.AUTH0_CLIENT_SECRET}",
-				"audience": "${process.env.AUTH0_AUDIENCE}",
-				"grant_type":"client_credentials"
-			}`
-		};
-		
-		return utilities.httpRequest(options).then(response => {
-			let parsedResponse = utilities.parseJSON(response);
-			return `Bearer ${parsedResponse.access_token}`;
-		});
-	}
+  getAccessToken: function (audience) {
+    let auth0Audience = process.env.AUTH0_AUDIENCE;
+    if(typeof audience !== 'undefined'){
+      auth0Audience = audience;
+    }
+    let options = {
+      method: 'POST',
+      url: `${process.env.AUTH0_DOMAIN}oauth/token`,
+      headers: { 'content-type': 'application/json' },
+      body: `{
+        "client_id": "${process.env.AUTH0_CLIENT_ID}",
+        "client_secret": "${process.env.AUTH0_CLIENT_SECRET}",
+        "audience": "${auth0Audience}",
+        "grant_type":"client_credentials"
+      }`
+    };
+    
+    return utilities.httpRequest(options).then(response => {
+      let parsedResponse = utilities.parseJSON(response);
+      return `Bearer ${parsedResponse.access_token}`;
+    });
+  }
 };
 
 module.exports = authorization;
